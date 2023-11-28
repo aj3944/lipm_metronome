217124a6c9cf9b2caa69b05fda02a98a524b34d0
aj3944 <aj3944@nyu.edu>
2023-11-27 20:00:44 -0500

    ready for step counting

--- gyro_lcd/src/main.cpp
+++ gyro_lcd/src/main.cpp
@@ -1,33 +1,42 @@
 #include "mbed.h"
 #include "../lib/LCD_DISCO/LCD_DISCO_F429ZI.h"
-#include <complex.h>
 
-SPI spi(PF_9, PF_8, PF_7); // mosi, miso, sclk
-DigitalOut cs(PC_1);
- 
+SPI spi(PF_9, PF_8, PF_7,PC_1,use_gpio_ssel); // mosi, miso, sclk, cs
 
+//address of first register with gyro data
+#define OUT_X_L 0x28
+#define REFERENCE 0x25
 
+//register fields(bits): data_rate(2),Bandwidth(2),Power_down(1),Zen(1),Yen(1),Xen(1)
+#define CTRL_REG1 0x20
 
-int writeReg(int address, int data){
-    cs = 0;
-    spi.write(address);
-    int reg_write = data;
-    spi.write(reg_write);
-    // printf("W [%x]=%x\n", address,reg_write);
-    cs = 1;
-    // thread_sleep_for(1);
-    return 1;
-}
+//configuration: 200Hz ODR,50Hz cutoff, Power on, Z on, Y on, X on
+#define CTRL_REG1_CONFIG 0b01'10'1'1'1'1
 
+//register fields(bits): data_rate(2),Bandwidth(2),Power_down(1),Zen(1),Yen(1),Xen(1)
+#define CTRL_REG2 0x21
 
-int readReg(int address){
-    cs = 0;
-    spi.write(address);
-    int reg_read = spi.write(0x00);
-    // printf("R [%x]=%x\n", address,reg_read);
-    cs = 1;
-    // thread_sleep_for(1);
-    return reg_read;
+//configuration: 200Hz ODR,50Hz cutoff, Power on, Z on, Y on, X on
+#define CTRL_REG2_CONFIG 0b00'00'0'1'1'1
+
+//register fields(bits): reserved(1), endian-ness(1),Full scale sel(2), reserved(1),self-test(2), SPI mode(1)
+#define CTRL_REG4 0x23
+
+//configuration: reserved,little endian,500 dps,reserved,disabled,4-wire mode
+#define CTRL_REG4_CONFIG 0b0'0'01'0'00'0
+
+#define SPI_FLAG 1
+
+uint8_t write_buf[32]; 
+uint8_t read_buf[32];
+
+EventFlags flags;
+//The spi.transfer() function requires that the callback
+//provided to it takes an int parameter
+void spi_cb(int event){
+  flags.set(SPI_FLAG);
+  
+
 }
 
 LCD_DISCO_F429ZI lcd;
@@ -71,41 +80,50 @@ void lcd_clear(){
 }
 
 
-
-
-
-
-
-
 int main()
 {
-    cs = 1;
     spi.format(8,3);
-    spi.frequency(1000000);
-    // INITALIZE I3D4250G
-    writeReg(0x20,0xFF);
-    writeReg(0x21,0x1A);
-    writeReg(0x2E,0x00);
+    spi.frequency(1'000'000);
+
+    write_buf[0]=CTRL_REG1;
+    write_buf[1]=CTRL_REG1_CONFIG;
+    spi.transfer(write_buf,2,read_buf,2,spi_cb,SPI_EVENT_COMPLETE );
+    flags.wait_all(SPI_FLAG);
+
+    // write_buf[0]=CTRL_REG2;
+    // write_buf[1]=CTRL_REG2_CONFIG;
+    // spi.transfer(write_buf,2,read_buf,2,spi_cb,SPI_EVENT_COMPLETE );
+    // flags.wait_all(SPI_FLAG);
+
+    write_buf[0]=CTRL_REG4;
+    write_buf[1]=CTRL_REG4_CONFIG;
+    spi.transfer(write_buf,2,read_buf,2,spi_cb,SPI_EVENT_COMPLETE );
+    flags.wait_all(SPI_FLAG); 
+
+
+      // write_buf[0]=REFERENCE|0x80|0x40;
+      // //start sequential sample reading
+      // spi.transfer(write_buf,7,read_buf,7,spi_cb,SPI_EVENT_COMPLETE );
+      // flags.wait_all(SPI_FLAG);
+      
 
     int out_vals[6];
-    int out_xyz[3];
+    float out_xyz[3];
 
 
 
-    int last_sane_xyz[3] = { 0 };
-    int smooth_xyz[3] = { 0 };
-    int normal_xyz[3] = { 0 };
-    int avg_xyz[240][3] = { 0 };
+    float last_sane_xyz[3] = { 0 };
+    float smooth_xyz[3] = { 0 };
+    float normal_xyz[3] = { 0 };
+    float avg_xyz[240][3] = { 0 };
     int t = 0;
     int rs = 10;
 
     int freq_max = 120;
 
-    complex<double> ring_ft_xyz[120][3] = { 0 };
 
-    lcd_clear();
+    // lcd_clear();
 
-    complex<double> freq_p,freq_o;
     int norm_freq;
     int val;
 
@@ -118,110 +136,54 @@ int main()
       if(i==2)     lcd.SetTextColor(LCD_COLOR_BLUE);
       lcd.DrawRect(0, 20+(i)*100, 239, 100*(i+1));
             
-      }
+    }
 
     // complex
     while(1)
     {
       t = t%235 + 1;
-      // if(t == 120){
-      //   lcd_clear();
 
-      // }
-      printf("\t\t\tT:%d\n",t);
-      for(int i = 0 ; i < 6; i++){
-        int ad = 0xA8 + i;
-        out_vals[i] = readReg(ad);
+      if(t == 1){
+        lcd_clear();
       }
+
+
+      int16_t raw_gx,raw_gy,raw_gz;
+      float gx, gy, gz;
+      //prepare the write buffer to trigger a sequential read
+      write_buf[0]=OUT_X_L|0x80|0x40;
+      //start sequential sample reading
+      spi.transfer(write_buf,7,read_buf,7,spi_cb,SPI_EVENT_COMPLETE );
+      flags.wait_all(SPI_FLAG);
+      //read_buf after transfer: garbage byte, gx_low,gx_high,gy_low,gy_high,gz_low,gz_high
+      //Put the high and low bytes in the correct order lowB,HighB -> HighB,LowB
+      raw_gx=( ( (uint16_t)read_buf[2] ) <<8 ) | ( (uint16_t)read_buf[1] );
+      raw_gy=( ( (uint16_t)read_buf[4] ) <<8 ) | ( (uint16_t)read_buf[3] );
+      raw_gz=( ( (uint16_t)read_buf[6] ) <<8 ) | ( (uint16_t)read_buf[5] );
+
+      //printf("RAW|\tgx: %d \t gy: %d \t gz: %d\t",raw_gx,raw_gy,raw_gz);
+
+      gx=((float)raw_gx)*(17.5f*0.017453292519943295769236907684886f / 1000.0f);
+      gy=((float)raw_gy)*(17.5f*0.017453292519943295769236907684886f / 1000.0f);
+      gz=((float)raw_gz)*(17.5f*0.017453292519943295769236907684886f / 1000.0f);
+      // printf("Actual|\tgx: %4.5f \t gy: %4.5f \t gz: %4.5f\n",gx,gy,gz);
+
+      out_xyz[0] = gx;
+      out_xyz[1] = gy;
+      out_xyz[2] = gz;
+
       for(int i = 0; i < 3; i++){
-        out_xyz[i] = out_vals[i] + (out_vals[i + 1] ^ 0x80) - (out_vals[i + 1] & 0x80);
-        last_sane_xyz[i] = out_xyz[i];
-        smooth_xyz[i] = (rs - 1)* smooth_xyz[i]/(rs) + last_sane_xyz[i]/(rs);
-        normal_xyz[i] = last_sane_xyz[i] - smooth_xyz[i];
+        // last_sane_xyz[i] = out_xyz[i];
+        // smooth_xyz[i] = (rs - 1)* smooth_xyz[i]/(rs) + last_sane_xyz[i]/(rs);
+        // normal_xyz[i] = last_sane_xyz[i] - smooth_xyz[i];
+        normal_xyz[i] = out_xyz[i];
         val = avg_xyz[t-1][i]/2 + normal_xyz[i]/2;
         avg_xyz[t][i] = val;
-
-        
-        // printf("val:%d \n",val);
-        // for(int w = 0 ; w < freq_max; w+=10){
-          // double expee = (double)avg_xyz[t][i] * (double)w;
-          // freq_p = complex<double>(0.0, expee);
-          // printf("FREQ_O:%d \n",avg_xyz[t][i] * w);
-
-          // freq_p = exp(freq_p);
-          // freq_o =  complex<double>(0, 0);
-          // freq_p =  exp(freq_o);
-          // freq_p =  freq_o;
-          // freq_p = complex<int>(real(freq_p),imag(freq_p));
-          // ring_ft_xyz[w][i] = freq_p;
-          // ring_ft_xyz[w][i] = complex(real(ing_ft_xyz[w][i])  + real(),);
-          // printf("W:%d O:(%d,%d)\t",w,real(freq_o),imag(freq_o));
-          // printf("P:(%f,%f)\t",real(freq_p),imag(freq_p));
-        // }
-        // printf("\n");
       }
-      printf("\n\n\n");
-
-      // for(int i = 0; i < 3; i++){
-      //   for(int w = 0 ; w < freq_max; w+=10){
-      //     // using namespace std::complex_literals;
-          // freq_p =  complex<int>(0,w);
-          // freq_p = exp(freq_p*avg_xyz[t][i]);
-      //     // freq_p = exp(freq_p)*avg_xyz[t][i];
-          // printf("AVGXYZ: %d T: %d\n",avg_xyz[t][i],t);
-      //     // printf("FREQ P: %d + i%d\n",real(freq_p),imag(freq_p));
-          // ring_ft_xyz[w][i] = complex(real(ing_ft_xyz[w][i])  + real(),;
-      //   }
-      // }
 
       for(int i = 0 ; i < 3; i++){
-        // char XYZ[3][20];
-        // snprintf(XYZ[i], sizeof(XYZ[i]), ">[%x]:%d\n",i,normal_xyz[i]);
-        // for(int w = 0 ; w < freq_max; w++){
-        //   lcd.DrawLine(w*2,graph_sector((int)real(ring_ft_xyz[w][i]),i),w*2,graph_sector((int)imag(ring_ft_xyz[w][i]),i));
-        // };
-        lcd.DrawLine(t,graph_sector(avg_xyz[t][i],i),t,graph_sector(avg_xyz[t][i],i)+1);
-        // printf(XYZ[i]);
+        lcd.DrawLine(t-1,graph_sector(avg_xyz[t-1][i]*100,i),t,graph_sector(avg_xyz[t][i]*100,i)+1);
       }
-
-
-
-      printf("\n");
-
+      thread_sleep_for(50);
     }
 }
-
-
-// GYRO_DISCO_F429ZI gyro;
-
-// int main()
-// {
-//   // thread_sleep_for(5);
-//   // printf("LOLALOLA");
-//   // float GyroBuffer[3];
-
-
-
-
-//   // writeReg(0x20,0xFF);
-//   // writeReg(0x21,0x17);
-//   // writeReg(0x2E,0x00);
-  
-
-
-
-//   // gyro.Init();
-  
-//   // printf("MOMAMOAM");
-//   while(1) {
-
-//       // printf("REDF");
-//       // Read Gyroscope values
-//       // gyro.GetXYZ(GyroBuffer);
-//       // //       Display values
-//       // // wait_us(50);
-//       // printf("X = %f\n", GyroBuffer[0]);
-//       // printf("Y = %f\n", GyroBuffer[1]);
-//       // printf("Z = %f\n", GyroBuffer[2]);
-//   }
-// }
