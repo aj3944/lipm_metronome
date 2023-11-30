eb23fe00ad065ae59bf5bf155cd52f052ac9d10a
aj3944 <aj3944@nyu.edu>
2023-11-30 03:22:35 -0500

    beatboxer

--- gyro_lcd/src/main.cpp
+++ gyro_lcd/src/main.cpp
@@ -1,7 +1,16 @@
 #include "mbed.h"
 #include "../lib/LCD_DISCO/LCD_DISCO_F429ZI.h"
+#include "../lib/Servo/Servo.h"
+
+
+#define PI 3.14159
 
 SPI spi(PF_9, PF_8, PF_7,PC_1,use_gpio_ssel); // mosi, miso, sclk, cs
+
+
+
+Servo myservo(PB_4);
+
 
 //address of first register with gyro data
 #define OUT_X_L 0x28
@@ -64,6 +73,19 @@ int graph_sector( int val, int axis){
 // }
 
 
+float goal_position = 0.;
+
+
+void move_servo(float goal_delta){
+    // for(float p=0; p<1.0; p += 0.1) {
+      // goal_position += goal_delta;
+      myservo.position(goal_delta);
+
+      printf("<<<SERVO MOVED TO %2.2f>>>\n",goal_position);
+      // myservo.position(0.5);
+      // thread_sleep_for(20);
+    // }
+}
 
 void lcd_clear(){
 
@@ -84,6 +106,9 @@ int main()
 {
     spi.format(8,3);
     spi.frequency(1'000'000);
+
+
+    myservo.calibrate(0.005,150);
 
     write_buf[0]=CTRL_REG1;
     write_buf[1]=CTRL_REG1_CONFIG;
@@ -107,28 +132,28 @@ int main()
       // flags.wait_all(SPI_FLAG);
       
 
-    int out_vals[6];
+    // int out_vals[6];
     float out_xyz[3];
 
 
 
-    float last_sane_xyz[3] = { 0 };
-    float smooth_xyz[3] = { 0 };
+    // float last_sane_xyz[3] = { 0 };
+    // float smooth_xyz[3] = { 0 };
     float normal_xyz[3] = { 0 };
     float avg_xyz[240][3] = { 0 };
     int t = 0;
-    int rs = 10;
+    // int rs = 10;
 
-    int freq_max = 120;
+    // int freq_max = 120;
 
 
     // lcd_clear();
 
-    int norm_freq;
+    // int norm_freq;
     int val;
 
 
-    double ang_veloc = 0.0;
+    // double ang_veloc = 0.0;
 
     for(int i = 0; i < 3; i++){
       if(i==0)     lcd.SetTextColor(LCD_COLOR_RED);
@@ -138,6 +163,11 @@ int main()
             
     }
 
+
+    float z_position = 0.5;
+    float error = 0.0;
+    float error_I = 0.0;
+    float error_D = 0.0;
     // complex
     while(1)
     {
@@ -168,6 +198,30 @@ int main()
       gz=((float)raw_gz)*(17.5f*0.017453292519943295769236907684886f / 1000.0f);
       // printf("Actual|\tgx: %4.5f \t gy: %4.5f \t gz: %4.5f\n",gx,gy,gz);
 
+      z_position += gz*180/PI;
+
+      printf("\t>GZ:%4.5f\n",gz);
+      printf("\t>ZPOS:%4.5f\n",z_position);
+      
+      // if( gz > 0.01 || 1){
+      //   // printf("\t\t\tGZ|\tposition: %4.5f \n",gz);
+      // }
+
+      error = 0.5 - z_position;
+      printf("\t\t error_b>:%4.5f\n",error);
+
+
+
+      float p = 2.5,i = 10, d =6;
+      if(error*error > 0.001){
+        float delta = error;
+        error -= delta;
+        printf("\t\t deltaL>:%4.5f\n",delta);
+        printf("\t\t error>:%4.5f\n",error);
+        move_servo(delta);
+      }
+
+
       out_xyz[0] = gx;
       out_xyz[1] = gy;
       out_xyz[2] = gz;
@@ -184,6 +238,8 @@ int main()
       for(int i = 0 ; i < 3; i++){
         lcd.DrawLine(t-1,graph_sector(avg_xyz[t-1][i]*100,i),t,graph_sector(avg_xyz[t][i]*100,i)+1);
       }
-      thread_sleep_for(50);
+      thread_sleep_for(5);
     }
 }
+
+
